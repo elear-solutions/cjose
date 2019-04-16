a18624042f2d660fff3a0b2c80d6994694520b60
deekshith <venkatadeekshith@elear.solutions>
2019-04-16 12:15:06 +0530

    Provides conan recipe to this library

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake, tools
+
+
+class CjoselibConan(ConanFile):
+    name = "cjose"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Elearcommonlib here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    requires="jansson/0.1@demo/dev", "openssl/0.1@demo/dev"
+    options = {"shared": [True, False]}
+    default_options = "shared=False","openssl:shared=True", "openssl:no_zlib=True"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        if (self.settings.os == "Android"):
+            cmake.definitions["Platform"] = "android"
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include/cjose", src="include/cjose")
+        self.copy("*", dst="lib",src="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["cjose"]
+        self.cpp_info.libs = ["cjose_static"]
+

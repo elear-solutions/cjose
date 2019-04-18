4b37a033657cc8eca4b810306b4411dcc58b5099
deekshith <venkatadeekshith@elear.solutions>
2019-04-18 14:32:52 +0530

    Eliminates indentation errors

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, CMake, tools
 
-
 class CjoselibConan(ConanFile):
     name = "cjose"
     version = "0.1"
@@ -10,15 +9,15 @@ class CjoselibConan(ConanFile):
     description = "<Description of Elearcommonlib here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    requires="jansson/0.1@demo/dev", "openssl/0.1@demo/dev"
+    requires="jansson/0.1@jenkins/master", "openssl/0.1@jenkins/master"
     options = {"shared": [True, False]}
-    default_options = "shared=False","openssl:shared=True", "openssl:no_zlib=True"
+    default_options = "shared=False", "openssl:no_asm=True"
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
         if (self.settings.os == "Android"):
-            cmake.definitions["Platform"] = "android"
+            cmake.definitions[ "Platform" ] = "android"
         cmake.configure(source_folder=".")
         cmake.build()
 
@@ -27,6 +26,5 @@ class CjoselibConan(ConanFile):
         self.copy("*", dst="lib",src="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["cjose"]
-        self.cpp_info.libs = ["cjose_static"]
-
+        self.cpp_info.libs = [ "cjose" ]
+        self.cpp_info.libs = [ "cjose_static" ]

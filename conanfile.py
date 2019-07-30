7cba586994f20fe503398daf337fcdf8ac6dd018
amiya-elear <amiyasantra@elear.solutions>
2019-07-30 13:25:51 +0530

    chnage in pkg version in conan file

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class CjoselibConan(ConanFile):
     description = "This recipe file used to build and package binaries of cjose repository"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    requires="jansson/0.1@jenkins/master", "openssl/0.1@jenkins/master"
+    requires="jansson/0.0.1@jenkins/master", "openssl/0.0.1@jenkins/master"
     options = {"shared": [True, False]}
     default_options = "shared=False", "openssl:no_asm=True"
     generators = "cmake"
